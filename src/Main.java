935b0a9195930264e48b0f4712bbcbced752ff8d
beka3301 <bekturhanazarov98@gmail.com>
2023-03-11 19:15:27 +0600

    upgrade commit

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println(" Ya toje praktikuyu");
         System.out.println("Privet!!!");
         System.out.println("AYDAR");
+        System.out.println("Men jonottum ");
     }
 }
