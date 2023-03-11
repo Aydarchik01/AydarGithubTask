f40135f19c6fd96c9538497e225d48c4c6116fd6
beka3301 <bekturhanazarov98@gmail.com>
2023-03-11 19:47:29 +0600

    upgrade commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Сафия");
-        System.out.println("привет");
-        System.out.println("Айдар");
-        System.out.println("dfghjk");
-        System.out.println("Почему игнориш?");
-        System.out.println("privet,vsem. chem zanyaty?");
+        System.out.println("");
     }
 }
